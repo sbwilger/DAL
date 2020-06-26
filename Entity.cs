ad71b984fd0cc6a4a4c04a1efdde0a75a6984637
Serana Wilger <sbwilger@gmail.com>
2020-06-26 08:38:33 -0400

    Add files via upload

---
+++ Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Serana Wilger
+/// 06/26/2020
+/// Entity.cs
+/// 
+/// The abstract class for Database Entities
+/// </summary>
+
+namespace sbwilger.DAL
+{
+    public abstract class Entity
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
