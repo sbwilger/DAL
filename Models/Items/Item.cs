9fb81500d8fd64c8630693de4f11d4da80c8ac2c
Serana Wilger <sbwilger@gmail.com>
2020-06-26 08:46:11 -0400

    Add files via upload
    Item class

---
+++ Models/Items/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Serana Wilger
+/// 06/26/2020
+/// Item.cs
+/// 
+/// A class for handling RPG items
+/// </summary>
+
+namespace sbwilger.DAL.Models.Items
+{
+    public class Item : Entity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
