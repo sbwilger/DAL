bb7a0e41934318b1afa1370c9133e6d4a3c480e8
Serana Wilger <sbwilger@gmail.com>
2020-06-26 15:32:06 -0400

    Add files via upload
    Added the class for storing entities in the database

---
+++ RPGContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sbwilger.DAL.Models.Items;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Serana Wilger
+/// 06/26/2020
+/// RPGContext.cs
+/// 
+/// Database storage for the RPG
+/// </summary>
+
+namespace sbwilger.DAL
+{
+    public class RPGContext : DbContext
+    {
+        public RPGContext(DbContextOptions<RPGContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
