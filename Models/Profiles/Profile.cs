48d09ff80fb0b5637888f29fa6f9268c22027198
Serana Wilger <sbwilger@gmail.com>
2020-07-06 22:35:58 -0400

    Add files via upload

---
+++ Models/Profiles/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sbwilger.DAL.Models.Profiles
+{
+    public class Profile : Entity
+    {
+        public ulong DiscordId { get; set; }
+        public ulong GuildId { get; set; }
+        public int Exp { get; set; }
+        public int ExpToLevel { get; set; }
+
+        public int Level { get; set; }
+    }
+}
