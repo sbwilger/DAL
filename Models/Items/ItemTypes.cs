dbf385649fe8366930af24122b23fdacff521dfe
Serana Wilger <sbwilger@gmail.com>
2020-07-09 22:13:11 -0400

    Add files via upload

---
+++ Models/Items/ItemTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sbwilger.DAL.Models.Items
+{
+    public enum ItemTypes
+    {
+        Ammunition,
+        Armour,
+        Consumable,
+        Pet,
+        Spell,
+        Weapon
+    }
+}
