9ed81806d1fd495695201f98e33826f4021af76d
Serana Wilger <sbwilger@gmail.com>
2020-07-10 23:39:16 -0400

    Add files via upload

---
+++ Models/BaseStat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sbwilger.DAL.Models
+{
+    public enum BaseStats
+    {
+        Strength,
+        Dexterity,
+        Constitution,
+        Intelligence,
+        Wisdom,
+        Luck
+    }
+}
