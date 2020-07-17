994bbd8a7cf8115a7dfe980c74a115c2b248bd6b
Serana Wilger <sbwilger@gmail.com>
2020-07-16 22:39:36 -0400

    Add files via upload

---
+++ Models/Items/Enchantment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sbwilger.DAL.Models.Items
+{
+    public enum EnchantmentTypes
+    {
+
+    }
+    public struct Enchantment
+    {
+        public EnchantmentTypes EnchantmentType { get; set; }
+        public int Potency { get; set; }
+        public int Duration { get; set; }
+    }
+}
