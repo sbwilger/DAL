dfa469a3edf4c133a0cfdd280505e5c58be6f9f6
Serana Wilger <sbwilger@gmail.com>
2020-07-26 23:33:33 -0400

    Add files via upload

--- Models/Items/PotionEffect.cs
+++ Models/Items/PotionEffect.cs
@@ -6,6 +6,20 @@ namespace sbwilger.DAL.Models.Items
 {
     public enum PotionEffect
     {
-
+        Cure_Disease,
+        Damage_Health,
+        Damage_Mana,
+        Damage_Mana_Regen,
+        Damage_Stamina,
+        Fear,
+        Fortify_Stamina,
+        Fortify_Magic,
+        Fortify_Block,
+        Fortify_Health,
+        Fortify_Heavy_Armour,
+        Fortify_Light_Armour,
+        Fortify_Mana,
+        Fortify_One_Handed,
+        Fortify_Two_Handed
     }
 }
