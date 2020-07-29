3956cc9d1c439eab8619bffa641601ce87edc191
Serana Wilger <sbwilger@gmail.com>
2020-07-28 23:47:06 -0400

    Add files via upload

---
+++ Ingredient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sbwilger.DAL.Models.Items
+{
+    class Ingredient : Item
+    {
+        public PotionEffect[] effects = new PotionEffect[4];
+    }
+}
