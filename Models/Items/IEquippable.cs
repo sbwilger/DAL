00e7ca0933e73c490684127658e5c1da132c5edf
Serana Wilger <sbwilger@gmail.com>
2020-07-30 23:43:17 -0400

    Add files via upload

---
+++ Models/Items/IEquippable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sbwilger.DAL.Models.Items
+{
+    public interface IEquippable
+    {
+        public bool equipped { get; set; }
+    }
+}
